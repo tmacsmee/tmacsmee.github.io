02e3142ee5942015f0daea8002e6d002151f1d03
tmacsmee <troymacsmee@gmail.com>
2024-07-20 22:23:46 +1200

    feat: add favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   redirects: async () => {
     return [{ source: "/", destination: "/about", permanent: true }]
   },
