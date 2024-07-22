8ca47e570ed6fcb9603147c8456f71d776d32ccc
tmacsmee <troymacsmee@gmail.com>
2024-07-23 00:45:22 +1200

    feat: add metadata files and generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://tmacsmee.github.io",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  output: "export",
+}
